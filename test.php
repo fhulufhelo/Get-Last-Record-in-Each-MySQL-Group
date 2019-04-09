b859b4c7bd910a76b27a43dbd724694b33e4a3f8
Fhulufhelo Mokhomi <mokhomifhulufhelo8@gmail.com>
2019-04-09 14:17:53 +0200

    Create test.php

---
+++ test.php
@@ -0,0 +1,17 @@
+$sql = "
+
+SELECT
+c.id, c.name,  c.email,
+r.id, r.companyid, r.name,  r.email
+FROM companytable c
+LEFT JOIN (
+ SELECT *
+ FROM revisiontable 
+ WHERE id 
+ IN ( SELECT MAX(id) 
+ FROM revisiontable 
+ GROUP BY companyid )
+)  r
+ON a.cid=b.r.id
+
+";
